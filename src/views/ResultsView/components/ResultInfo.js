8dd4a7d8ffb465510bfbab899924d886aa8207a0
amrouda50 <amrouda50@hotmail.com>
2024-06-12 03:25:48 +0300

    redesign for left hand side best destinations done

--- src/views/ResultsView/components/ResultInfo.js
+++ src/views/ResultsView/components/ResultInfo.js
@@ -39,7 +39,7 @@ const ResultInfo = ({ country, label }) => {
   }
 
   return (
-    <div className="dark-theme">
+    <div className="white-theme">
       <PieChartComponent
         scores={scores}
         label={label}
