7dde8e4776a89a688e7bcea97644615149bbf9f0
amrouda50 <amrouda50@hotmail.com>
2024-06-17 05:08:51 +0300

    right padding done for scroll

--- src/views/ResultsView/RightSidebar.js
+++ src/views/ResultsView/RightSidebar.js
@@ -29,7 +29,7 @@ export const RightSidebar = ({ activeResult}) => {
   }, [activeResult]);
 
   return (
-    <div className={'py-2 h-100 overflow-y-scroll'}>
+    <div className={'py-2 pe-2 h-100 overflow-y-scroll'}>
       <LogButton/>
       <p className={'m-0'} style={{ textAlign: "left" }}>
         Best destinations for {capitalize(user?.username ?? "you")}
